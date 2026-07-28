e923a4edaa61d06475d710a61ca9406de099aaec
Natanael Lourenço <holasoycael@gmail.com>
2026-07-27 21:37:22 -0300

    feat: add GITHUB_TOKEN env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,5 +6,8 @@ import env from 'utils/env'
 export const runtime = 'nodejs'
 
 export default axios.create({
-  baseURL: env.GITHUB_API
+  baseURL: env.GITHUB_API,
+  headers: {
+    ...(env.GITHUB_TOKEN ? { Authorization: `Bearer ${env.GITHUB_TOKEN}` } : {})
+  }
 })
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod'
 
 const schema = z.object({
-  GITHUB_API: z.string()
+  GITHUB_API: z.string(),
+  GITHUB_TOKEN: z.string().optional()
 })
 
 export default schema.parse({
-  GITHUB_API: import.meta.env.VITE_GITHUB_API
+  GITHUB_API: import.meta.env.VITE_GITHUB_API,
+  GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN
 })
